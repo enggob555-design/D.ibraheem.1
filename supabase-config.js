b8a72b84fe841b7643013b599a4c3ea81c4d48ba
enggob555-design <enggob555@gmail.com>
2026-06-09 02:27:34 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
-// 🌐 ملف الإعدادات الموحد لمنصة العيادة الذكية
+// ملف الإعدادات المركزي لمشروع عيادتي
 const SUPABASE_URL = "https://dpyshuhinqruhaqftydkl.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRweXNodWhpbnFueGhhcWZkeWtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4Mzc4ODQsImV4cCI6MjA5NjQxMzg4NH0.TpHanzVxjMEOJM0P3Oioic_odTmp7JJ-fFzBblOJVs0";
+const SUPABASE_ANON_KEY = "EyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRweXNodWhpbnFueGhhcWZkeWtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4Mzc4ODQsImV4cCI6MjA5NjQxMzg4NH0.TpHanzVxjMEOJM0P3Oioic_odTmp7JJ-fFzBblOJVs0"; 
 
-// تهيئة العميل الموحد للمشروع كامل برمجياً
+// تهيئة مكتبة سوبابيس وجعلها متاحاً للنظام ككل
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
