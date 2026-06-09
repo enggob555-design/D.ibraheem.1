44a4140cf7d86f11e3fa21682912397dfc50046c
enggob555-design <enggob555@gmail.com>
2026-06-09 05:00:32 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
 const SUPABASE_URL = "https://dpyshuhinqnxhaqfdykl.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRweXNodWhpbnFueGhhcWZkeWtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4Mzc4ODQsImV4cCI6MjA5NjQxMzg4NH0.TpHanzVxjMEOJM0P3Oioic_odTmp7JJ-fFzBblOJVs0";
 
-// إنشاء العميل
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// تعريف المتغير global ليراه ملف index.html بوضوح
+window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
