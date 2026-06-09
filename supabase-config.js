6a93d65512819d36a5b8fd761d11dcd4aea15fb5
enggob555-design <enggob555@gmail.com>
2026-06-09 03:55:49 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,6 @@
 // ملف الإعدادات المركزي الموحد والمحمي - مشروع عيادتي
-const SUPABASE_URL = "https://dpyshuhinqruhaqftydkl.supabase.co";
+const SUPABASE_URL = "const SUPABASE_URL = "https://dpyshuhinqnxhaqfdykl.supabase.co";
+";
 const SUPABASE_ANON_KEY = "EyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRweXNodWhpbnFueGhhcWZkeWtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4Mzc4ODQsImV4cCI6MjA5NjQxMzg4NH0.TpHanzVxjMEOJM0P3Oioic_odTmp7JJ-fFzBblOJVs0";
 
 // فحص أمان وجود المكتبة قبل تفعيل الاتصال
